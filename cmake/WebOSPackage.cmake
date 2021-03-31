f018598d6d68b1ed85d8c545cbb0f3875590db96
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-04-01 00:46:32 +0900

    Installation target device

--- cmake/WebOSPackage.cmake
+++ cmake/WebOSPackage.cmake
@@ -76,17 +76,37 @@ function(target_webos_package TARGET)
     )
     
     set(ares_arguments "")
-    if (WEBOS_INSTALL_DEVICE)
-        set(ares_arguments "-d" ${WEBOS_INSTALL_DEVICE})
+
+    if (WEBOS_INSTALL_DEVICES_LIST)
+        execute_process(COMMAND bash -c "ares-setup-device -F | json -a name" OUTPUT_VARIABLE ares_devices_output)
+        string(REPLACE "\n" ";" ares_devices_list ${ares_devices_output})
+        # separate_arguments(ares_devices_list)
+        foreach(ares_device_name ${ares_devices_list})
+            set(ares_arguments "-d" ${ares_device_name})
+            
+            add_custom_target(webos-install-${TARGET}-${ares_device_name}
+                COMMAND ares-install ${ares_arguments} ${CMAKE_CURRENT_BINARY_DIR}/${package_ipk_name}
+                DEPENDS webos-package-${TARGET}
+            )
+
+            add_custom_target(webos-launch-${TARGET}-${ares_device_name}
+                COMMAND ares-launch ${appinfo_id} ${ares_arguments} 
+                DEPENDS webos-install-${TARGET}-${ares_device_name}
+            )
+        endforeach()
+    else()
+        if (WEBOS_INSTALL_DEVICE)
+            set(ares_arguments "-d" ${WEBOS_INSTALL_DEVICE})
+        endif()
+
+        add_custom_target(webos-install-${TARGET}
+            COMMAND ares-install ${ares_arguments} ${CMAKE_CURRENT_BINARY_DIR}/${package_ipk_name}
+            DEPENDS webos-package-${TARGET}
+        )
+
+        add_custom_target(webos-launch-${TARGET}
+            COMMAND ares-launch ${appinfo_id} ${ares_arguments} 
+            DEPENDS webos-install-${TARGET}
+        )
     endif()
-
-    add_custom_target(webos-install-${TARGET}
-        COMMAND ares-install ${ares_arguments} ${CMAKE_CURRENT_BINARY_DIR}/${package_ipk_name}
-        DEPENDS webos-package-${TARGET}
-    )
-
-    add_custom_target(webos-launch-${TARGET}
-        COMMAND ares-launch ${appinfo_id} ${ares_arguments} 
-        DEPENDS webos-install-${TARGET}
-    )
 endfunction()

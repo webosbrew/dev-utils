002037dcd62d590211f3e354d82f60f64159ab8b
Mariotaku <mariotaku.lee@gmail.com>
2022-01-02 21:00:11 +0900

    simplified cmake wrapper

--- scripts/webos-cmake-wrapper.sh
+++ scripts/webos-cmake-wrapper.sh
@@ -2,11 +2,4 @@
 
 . /opt/webos-sdk-x86_64/1.0.g/environment-setup-armv7a-neon-webos-linux-gnueabi
 
-CMAKE_TOOLCHAIN_OPT="-DCMAKE_TOOLCHAIN_FILE=$OECORE_NATIVE_SYSROOT/usr/share/cmake/OEToolchainConfig.cmake"
-CMAKE_COMMAND=$(which cmake)
-
-if [ '--build' = "$1" ]; then
-  CMAKE_TOOLCHAIN_OPT=""
-fi
-
-$CMAKE_COMMAND "$CMAKE_TOOLCHAIN_OPT" "$@"
+cmake "$@"

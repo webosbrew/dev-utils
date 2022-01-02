68c4dbadf52935510c7ead199ca3f40db786f75c
Mariotaku <mariotaku.lee@gmail.com>
2022-01-02 21:43:49 +0900

    proper cmake wrapper implementation

--- scripts/webos-cmake-wrapper.sh
+++ scripts/webos-cmake-wrapper.sh
@@ -2,4 +2,27 @@
 
 . /opt/webos-sdk-x86_64/1.0.g/environment-setup-armv7a-neon-webos-linux-gnueabi
 
-cmake "$@"
+CMAKE_BIN=$(which cmake)
+
+IS_CONFIGURE=1
+while getopts ":t:-:" arg; do
+  case $arg in
+  t) IS_CONFIGURE=0 ;;
+  -)
+    case $OPTARG in
+    build) IS_CONFIGURE=0 ;;
+    install) IS_CONFIGURE=0 ;;
+    target) IS_CONFIGURE=0 ;;
+    *) ;;
+    esac
+    ;;
+  *) ;;
+  esac
+done
+
+if [ ${IS_CONFIGURE} = 1 ]; then
+  toolchain_opt="-DCMAKE_TOOLCHAIN_FILE=$OECORE_NATIVE_SYSROOT/usr/share/cmake/OEToolchainConfig.cmake"
+  exec "$CMAKE_BIN" "${toolchain_opt}" "$@"
+else
+  exec "$CMAKE_BIN" "$@"
+fi
